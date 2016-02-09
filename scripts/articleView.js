4fb8ea63f39d5e907a7e74d7afcfefc27e50b0d3
Rob Van Leuven <114.robert@gmail.com>
2016-02-08 23:25:50 -0800

    It's working in the console but nothing is loading. Sick of banging my head against the wall with this and too many git issues so I'm turning it in even though it doesn't fully work.

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -22,7 +22,6 @@ articleView.handleCategoryFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
-    $('#author-filter').val('');
   });
 };
 
@@ -45,9 +44,49 @@ articleView.setTeasers = function() {
   });
 };
 
-$(document).ready(function() {
+articleView.initNewArticlePage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+
+  $('#new-form').on('change', 'input, textarea', articleView.create);
+};
+
+articleView.create = function() {
+  var article;
+  $('#articles').empty();
+
+  // Instantiate an article based on what's in the form fields:
+  article = new Article({
+    title: $('#article-title').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? util.today() : null
+  });
+
+  // Use the Handblebars template to put this new article into the DOM:
+  $('#articles').append(article.toHtml());
+
+  // Activate the highlighting of any code blocks:
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
+  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(article) + ',');
+};
+
+
+articleView.initIndexPage = function() {
+  Article.all.forEach(function(a){
+    $('#articles').append(a.toHtml())
+  });
+
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
-})
+};
