faf145f391f47e6d89ba0dd5dcb2ee8e7da59813
Rob Van Leuven <114.robert@gmail.com>
2016-03-07 00:26:19 -0800

    Uploading IIFEs/modifications/code review.

--- scripts/article.js
+++ scripts/article.js
@@ -1,62 +1,70 @@
-var articles = [];
+(function(module) {
 
-function Article (opts) {
-  Object.keys(opts).forEach(function(e, index, keys) {
-    this[e] = opts[e];
-  },this);
-}
+  var articles = [];
 
-Article.all = [];
+  function Article (opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  }
 
-Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
-  this.body = marked(this.body);
+  Article.all = [];
 
-  return template(this);
-};
+  Article.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
+    this.body = marked(this.body);
 
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
+    return template(this);
+  };
 
-  rawData.forEach(function(ele) {
-    Article.all.push(new Article(ele));
-  })
-}
-
-Article.getAll = function(rawData) {
-  $.getJSON('data/portdata.json', function(rawData) {
-    console.log('loading json anew');
-    localStorage.rawData = JSON.stringify(rawData);
-    Article.loadAll(rawData);
-    articleView.initIndexPage();
-  });
-}
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-Article.fetchAll = function() {
-  if (localStorage.rawData) {
-    $.ajax({
-      type: 'HEAD',
-      url: 'data/portdata.json',
-      success: function(data,message,xhr) {
-        console.log(xhr);
-        var eTag = xhr.getResponseHeader('eTag');
-        if(!localStorage.eTag || eTag !== localStorage.eTag) {
-          localStorage.eTag = eTag;
-          Article.getAll();
-          console.log('changed etag load');
-        } else {
-          Article.loadAll(JSON.parse(localStorage.rawData));
-          articleView.initIndexPage();
-          console.log('Loaded from LS');
-        }
-      }
+// I feel like this function could cause some problems if it gets called more than once. It's pushing to the Article.all array instead of setting its value.
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     });
 
-  } else {
-    Article.getAll();
+    rawData.forEach(function(ele) {
+      Article.all.push(new Article(ele));
+    })
   }
-}
+
+// This getAll function seems like it could be merged into the fetchAll function once an SQL table is impelmented.
+  Article.getAll = function(rawData) {
+    $.getJSON('data/portdata.json', function(rawData) {
+      console.log('loading json anew');
+      localStorage.rawData = JSON.stringify(rawData);
+      Article.loadAll(rawData);
+      articleView.initIndexPage();
+    });
+  }
+
+  // This function will retrieve the data from either a local or remote source,
+  // and process it, then hand off control to the View.
+  // This should use a SQL table to be more efficient.
+  Article.fetchAll = function() {
+    if (localStorage.rawData) {
+      $.ajax({
+        type: 'HEAD',
+        url: 'data/portdata.json',
+        success: function(data,message,xhr) {
+          console.log(xhr);
+          var eTag = xhr.getResponseHeader('eTag');
+          if(!localStorage.eTag || eTag !== localStorage.eTag) {
+            localStorage.eTag = eTag;
+            Article.getAll();
+            console.log('changed etag load');
+          } else {
+            Article.loadAll(JSON.parse(localStorage.rawData));
+            articleView.initIndexPage();
+            console.log('Loaded from LS');
+          }
+        }
+      });
+
+    } else {
+      Article.getAll();
+    }
+  }
+
+  module.articlesController = articlesController;
+})(window);
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,55 +1,58 @@
+(function(module) {
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
-var articleView = {};
+  var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).attr('data-category');
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
+  articleView.populateFilters = function() {
+    $('article').each(function() {
+      if (!$(this).hasClass('template')) {
+        var val = $(this).attr('data-category');
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        if ($('#category-filter option[value="' + val + '"]').length === 0) {
+          $('#category-filter').append(optionTag);
+        }
       }
-    }
-  });
-};
+    });
+  };
 
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-category="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-  });
-};
+  articleView.handleCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-category="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('article').fadeIn();
+        $('article.template').hide();
+      }
+    });
+  };
 
-articleView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(e) {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
+  articleView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function(e) {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
 
-  $('.main-nav .tab:first').click();
-};
+    $('.main-nav .tab:first').click();
+  };
 
-articleView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  console.log("Teasers set");
-  $('#articles').on('click', 'a.read-on', function(e) {
-    e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).hide();
-  });
-};
+  articleView.setTeasers = function() {
+    $('.article-body *:nth-of-type(n+2)').hide();
+    console.log("Teasers set");
+    $('#articles').on('click', 'a.read-on', function(e) {
+      e.preventDefault();
+      $(this).parent().find('*').fadeIn();
+      $(this).hide();
+    });
+  };
 
-articleView.initIndexPage = function() {
-  Article.all.forEach(function(a){
-    $('#articles').append(a.toHtml())
-  });
-  articleView.populateFilters();
-  articleView.handleCategoryFilter();
-  articleView.handleMainNav();
-  articleView.setTeasers();
-}
+  articleView.initIndexPage = function() {
+    Article.all.forEach(function(a){
+      $('#articles').append(a.toHtml())
+    });
+    articleView.populateFilters();
+    articleView.handleCategoryFilter();
+    articleView.handleMainNav();
+    articleView.setTeasers();
+  }
+  module.articlesController = articlesController;
+})(window);
